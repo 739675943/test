f6a2fb61bf7ecb3e8bdb489dfc2e7b7158916785
739675943 <739675943@qq.com>
2018-01-03 23:34:47 +0800

    first commit

--- AdjMGraph.h
+++ AdjMGraph.h
@@ -16,7 +16,7 @@ typedef struct
     int weight;  //权值
 }RowColWeight;//边信息结构体定义
 
-//置带权有向图G为空图
+//初始化
 void GraphInitiate(AdjMGraph *G)
 {
 	int i,j;
@@ -41,7 +41,7 @@ void GraphInitiate(AdjMGraph *G)
 
 
 
-//在带权有向图G中插入顶点vertex。如果图中已经有顶点vertex,则图不变，时间复杂度:O(n)。
+//在带权有向图G中插入顶点vertex
 void InsertVertex(AdjMGraph *G,DataType vertex)
 {
 	//if(IsVertex(G,vertex)<0)
@@ -52,11 +52,8 @@ void InsertVertex(AdjMGraph *G,DataType vertex)
 		}
 }
 
-/* 在带权有向图G中插入一条第v1个顶点指向第v2个顶点，权值为weight的有向边。 
-* 如果v1和v2有一个不是图中的顶点，则图不变；如果v1和v2相等，则图不变。
-* 如果图已经包含该边，则边的权值更改为新的权值，时间复杂度:O(1)。
-上面插入的是有向边，我们插入无向边的时候可以进行两次的有向边的插入。
-*/
+// 在带权有向图G中插入一条第v1个顶点指向第v2个顶点，权值为weight的有向边 
+
 void InsertEdge(AdjMGraph *G,int v1,int v2,int weight)
 {
 	DataType x;
@@ -76,7 +73,7 @@ void InsertEdge(AdjMGraph *G,int v1,int v2,int weight)
 
 
 
-// 在带权有向图G中删除一条第v1个顶点指向第v2个顶点的边。
+// 在带权有向图G中删除一条第v1个顶点指向第v2个顶点的边
 
 void DeleteEdge(AdjMGraph *G,int v1,int v2)
 {
@@ -93,7 +90,7 @@ void DeleteEdge(AdjMGraph *G,int v1,int v2)
 
 
 //删除顶点
-//在带权有向图G中删除第v个顶点，时间复杂度:O(n^2)。
+//在带权有向图G中删除第v个顶点
 void DeleteVertex(AdjMGraph *G,int v)
 {
 	
