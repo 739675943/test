26bbb783457f301d1bd0a7c44dcb5883fce639d9
739675943 <739675943@qq.com>
2018-01-03 23:38:37 +0800

    first commit

--- AdjMGraph.h
+++ AdjMGraph.h
@@ -103,19 +103,13 @@ void DeleteVertex(AdjMGraph *G,int v)
 		   }
 		   else 
 		   { 
-			   
-	//		   for(i=m3;i<G->vertices.size;i++)
+
 				   for(j=0,i=m3;j<G->vertices.size;j++)
 				   {
                        G->edge[j][i]=MaxWeight;
 
 				   }
-	//			  for(i=m3;i<G->vertices.size;i++)
 					  for(i=m3,j=0;j<G->vertices.size;j++)
 						 G->edge[i][j]=MaxWeight;
-	//		   for(i=m3;i<G->vertices.size;i++)
-//				   G->vertices.list[i]=G->vertices.list[i]-1;
-	//		       G->vertices.size--;
-	//			printf("删除结点成功\n");
 		   }
 }
