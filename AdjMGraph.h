980abd2c1ccd8afc21216a4785af5c99c0b1a1cd
739675943 <739675943@qq.com>
2018-01-08 09:46:41 +0800

    first commit

--- AdjMGraph.h
+++ AdjMGraph.h
@@ -113,3 +113,108 @@ void DeleteVertex(AdjMGraph *G,int v)
 						 G->edge[i][j]=MaxWeight;
 		   }
 }
+
+int GetFirstVex(AdjMGraph G,int v)
+{
+    int col;DataType x;
+	v=v-1;
+    
+    if(ListGet(G.vertices,v,&x)==0)
+    {
+        printf("取第一个邻接顶点时参数v越界出错！\n");
+        exit(1);
+	}
+	
+	for(col=0;col<G.vertices.size;col++)
+        if(G.edge[v][col]>0 && G.edge[v][col] < MaxWeight) 
+			return col;
+		return -1; 
+}
+
+int GetNextVex(AdjMGraph G,int v1,int v2)
+{
+    int col;DataType x;
+    
+    if((ListGet(G.vertices,v1,&x)==0)||(ListGet(G.vertices,v2,&x)==0))
+    {
+        printf("取下一邻接顶点时参数v1和v2越界出错！\n");
+        exit(1);
+	}
+	if(G.edge[v1][v2]==0)
+	{
+		printf("v2不是v1的邻接顶点\n");
+        exit(1);
+	}
+	for(col=v2+1;col<G.vertices.size;col++)
+        if(G.edge[v1][col]>0 && G.edge[v1][col]<MaxWeight) 
+			return col;
+        return -1;
+}1
+void CreatGraph(AdjMGraph *G,DataType v[],int n,RowColWeight W[],int e)
+{
+	int i,k;
+	
+	GraphInitiate(G);
+	for(i=0;i<n;i++)
+	{
+			//cout<<n<<endl;
+			InsertVertex(G,v[i]);
+	}
+	for(k=0;k<e;k++)
+        InsertEdge(G,W[k].row,W[k].col,W[k].weight);
+}
+
+
+
+
+void Dijkstra(AdjMGraph *G, int v0, int distance[], int path[ ])
+{ 
+	int n = G->vertices.size;
+	int *S = (int *)malloc(sizeof(int)*n); //S数组
+	int minDis, i, j, u;
+    	FILE *fp;  
+	/*初始化*/
+	for(i = 0; i < n; i ++)
+	{ 
+		distance[i] = G->edge[v0][i];
+		S[i] = 0;
+		if(i != v0 && distance[i] < MaxWeight)
+			path[i] = v0;
+		else path[i] = -1;
+	}
+	S[v0] = 1;
+	for(i = 1; i < n; i ++)
+	{ 
+		minDis = MaxWeight;
+		for (j = 0;j < n;j ++)
+			if(S[j] == 0 && distance[j] < minDis)
+			{
+				u = j;
+				minDis = distance[j];
+			}
+			if(minDis == MaxWeight) return;
+			S[u] = 1; 
+			for(j = 0; j < n; j++)
+				if(S[j] == 0 && G->edge[u][j] < MaxWeight &&
+					distance[u] + G->edge[u][j] < distance[j])
+				{  
+					distance[j] = distance[u] + G->edge[u][j];
+					path[j] = u;
+				}
+	}
+	printf("目的路由  下一跳路由\n");
+	 fp=fopen("luyoubiao.txt","w");
+	for(i=0;i<n;i++)
+	{
+		if (i==v0) continue;
+		j=i;
+		while(path[j]!=v0)
+		{
+			j=path[j];
+			if (j==-1) break;
+		}
+		printf("%5d%12d\n",i+1,j+1);
+        fprintf(fp,"%5d%12d\n",i+1,j+1);
+	}
+	fclose(fp);
+}
